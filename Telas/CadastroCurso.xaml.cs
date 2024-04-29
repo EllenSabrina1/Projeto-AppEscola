143174f2d4461d6daf2825805d9ba128c6ef4127
Ellen Sabrina <ellen.pereira@estudante.ifro.edu.br>
2024-04-29 16:00:38 -0400

    primeiro commit

---
+++ Telas/CadastroCurso.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AppEscolaa.Telas
+{
+    /// <summary>
+    /// Lógica interna para CadastroCurso.xaml
+    /// </summary>
+    public partial class CadastroCurso : Window
+    {
+        public CadastroCurso()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Dados salvos com sucesso!");
+            this.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Telas.inicio mainWindow = new Telas.inicio();
+            mainWindow.ShowDialog();
+            this.Close();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Telas.cadastroAluno mainWindow = new Telas.cadastroAluno();
+            mainWindow.ShowDialog();
+            this.Close();
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            CadastroEscola mainWindow = new Telas.CadastroEscola();
+            mainWindow.ShowDialog();
+            this.Close();
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            CadastroCurso mainWindow = new Telas.CadastroCurso();
+            mainWindow.ShowDialog();
+            this.Close();
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            CadastroTurma mainWindow = new Telas.CadastroTurma();
+            mainWindow.ShowDialog();
+            this.Close();
+        }
+
+        private void Button_Click_6(object sender, RoutedEventArgs e)
+        {
+            Telas.ConsultaAluno mainWindow = new Telas.ConsultaAluno();
+            mainWindow.ShowDialog();
+            this.Close();
+        }
+
+        private void Button_Click_7(object sender, RoutedEventArgs e)
+        {
+            Telas.ConsultaTurma mainWindow = new Telas.ConsultaTurma();
+            mainWindow.ShowDialog();
+            this.Close();
+        }
+
+        private void Button_Click_8(object sender, RoutedEventArgs e)
+        {
+            Telas.ConsultaCurso mainWindow = new Telas.ConsultaCurso();
+            mainWindow.ShowDialog();
+            this.Close();
+        }
+
+        private void Button_Click_9(object sender, RoutedEventArgs e)
+        {
+            Telas.ConsultaEscola mainWindow = new Telas.ConsultaEscola();
+            mainWindow.ShowDialog();
+            this.Close();
+        }
+
+        private void Button_Click_10(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Button_Click_11(object sender, RoutedEventArgs e)
+        {
+            inicio mainWindow = new Telas.inicio();
+            mainWindow.ShowDialog();
+            this.Close();
+        }
+    }
+}
---
+++ Telas/ConsultaAluno.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AppEscolaa.Telas
+{
+    /// <summary>
+    /// Lógica interna para ConsultaAluno.xaml
+    /// </summary>
+    public partial class ConsultaAluno : Window
+    {
+        public ConsultaAluno()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_9(object sender, RoutedEventArgs e)
+        {
+            Telas.inicio mainWindow = new Telas.inicio();
+            mainWindow.ShowDialog();
+            this.Close();
+        }
+    }
+}
